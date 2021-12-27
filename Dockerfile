fbae633dcbf90be61e3a78165d1b2aa60eae78fc
selvakumar-altascio <68061068+selvakumar-altascio@users.noreply.github.com>
2021-12-27 16:33:06 +0530

    Test

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,18 @@
 # https://hub.docker.com/_/microsoft-dotnet
 
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
-ADD WebApplication2 /source
+WORKDIR /source
 
 # copy csproj and restore as distinct layers
 COPY *.sln .
+COPY WebApplication2/*.csproj ./WebApplication2/
+
 RUN dotnet restore
+
+# copy everything else and build app
+COPY WebApplication2/. ./WebApplication2/
+
+WORKDIR /source/WebApplication2
 
 RUN dotnet publish -c release -o /app --no-restore
 
--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+//using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,10 +27,10 @@ namespace WebApplication2
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication2", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication2", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +39,8 @@ namespace WebApplication2
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication2 v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication2 v1"));
             }
 
             app.UseRouting();
